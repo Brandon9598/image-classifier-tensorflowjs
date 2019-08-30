06eff4936b8331fe291cf43729f8236b3374a5ee
Brandon Hancock <brandon9598@gmail.com>
2019-08-30 14:58:06 -0400

    image uploaded working

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
+// Tensorflow.js
 let net;
-const classifier = knnClassifier.create();
-const webcamElement = document.getElementById('webcam');
+
+// HTML Elements
+const imageInput = document.getElementById('file-input');
+const displayImg = document.getElementById('displayImg')
+const predictionConsole = document.getElementById('prediction-console')
 
 async function app() {
   console.log('Loading mobilenet..');
@@ -9,43 +13,21 @@ async function app() {
   net = await mobilenet.load();
   console.log('Sucessfully loaded model');
 
-  await setupWebcam();
-  while(true){
-    const result = await net.classify(webcamElement);
-
-    document.getElementById('console').innerText = `
-      prediction: ${result[0].className}\n
-      probability: ${result[0].probability}
-    `;
-
-    // Give some breathing room by waiting for the next animation frame to
-    // fire.
-    await tf.nextFrame();
-  }
-
   // Make a prediction through the model on our image.
-  const imgEl = document.getElementById('img');
-  const result = await net.classify(imgEl);
+  const result = await net.classify(displayImg);
+  result.forEach(function(prediction, index){
+      console.log("Prediction", prediction)
+      console.log("index", index)
+      const predictionElement = document.createElement('h2');
+      predictionElement.appendChild(document.createTextNode(`${index + 1}. ${prediction.className} - ${Math.round(prediction.probability * 100)}%`))
+    predictionConsole.appendChild(predictionElement)
+  })
   console.log(result);
 }
 
-async function setupWebcam() {
-    return new Promise((resolve, reject) => {
-        const navigatorAny = navigator;
-        navigator.getUserMedia = navigator.getUserMedia ||
-            navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
-            navigatorAny.msGetUserMedia;
-        if (navigator.getUserMedia) {
-        navigator.getUserMedia({video: true},
-            stream => {
-            webcamElement.srcObject = stream;
-            webcamElement.addEventListener('loadeddata',  () => resolve(), false);
-            },
-            error => reject());
-        } else {
-        reject();
-        }
-    });
-}
+imageInput.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    displayImg.src = URL.createObjectURL(file)
+    app();
+})
 
-app();
